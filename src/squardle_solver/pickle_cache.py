651648644fd1ab1cd5f74611142ed2bef2593639
jakelevi1996 <jakelevi@hotmail.co.uk>
2025-01-02 12:59:55 +0000

    Display correct saving/loading information to `STDOUT`

--- src/squardle_solver/pickle_cache.py
+++ src/squardle_solver/pickle_cache.py
@@ -5,18 +5,20 @@ from jutility import util
 class PickleCache:
     def load(self):
         full_path = self.get_path()
-        dir_name  = os.path.dirname(full_path)
-        print("Loading from \"%s\"" % full_path)
 
         if not os.path.isfile(full_path):
             with util.Timer("PickleCache.make"):
                 data = self.make()
-            if not os.path.isdir(dir_name):
-                os.makedirs(dir_name)
+
+            print("Saving in \"%s\"" % full_path)
             with util.Timer("PickleCache.save"):
+                dir_name  = os.path.dirname(full_path)
+                if not os.path.isdir(dir_name):
+                    os.makedirs(dir_name)
                 with open(full_path, "wb") as f:
                     pickle.dump(data, f)
         else:
+            print("Loading from \"%s\"" % full_path)
             with util.Timer("PickleCache.load"):
                 data = util.load_pickle(full_path)
 
